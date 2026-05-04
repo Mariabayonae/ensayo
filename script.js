01a21b137eb90d04986510fc700d372020add362
Mariabayonae <bayonam346@gmail.com>
2026-05-04 15:18:49 +0000

    intento 5

--- script.js
+++ script.js
@@ -1,33 +1,28 @@
 const places = [
 {
-title:"Giethoorn",
+title:"Giethoorn, Países Bajos",
 img:"https://images.unsplash.com/photo-1500375592092-40eb2168fd21",
-desc:"Pueblo sin carreteras, solo canales.",
-color:"#dbeafe"
+desc:"Un pueblo completamente sin carreteras. La vida se mueve en botes por canales. Es conocido como la ‘Venecia del norte’ por su tranquilidad y paisaje único."
 },
 {
-title:"Chefchaouen",
+title:"Chefchaouen, Marruecos",
 img:"https://images.unsplash.com/photo-1524492412937-b28074a5d7da",
-desc:"Ciudad azul en Marruecos.",
-color:"#c7d2fe"
+desc:"Ciudad pintada completamente de azul. Sus calles, paredes y escaleras crean un ambiente único en las montañas del Rif."
 },
 {
-title:"Socotra",
+title:"Isla Socotra, Yemen",
 img:"https://images.unsplash.com/photo-1507525428034-b723cf961d3e",
-desc:"Paisajes únicos del mundo.",
-color:"#fde68a"
+desc:"Una isla con especies únicas en el mundo. Sus árboles parecen de otro planeta y su paisaje es casi extraterrestre."
 },
 {
-title:"Huacachina",
+title:"Huacachina, Perú",
 img:"https://images.unsplash.com/photo-1501785888041-af3ef285b470",
-desc:"Oasis en el desierto.",
-color:"#fed7aa"
+desc:"Un oasis natural rodeado de enormes dunas de arena. Es uno de los pocos oasis naturales en Sudamérica."
 },
 {
-title:"Hallstatt",
+title:"Hallstatt, Austria",
 img:"https://images.unsplash.com/photo-1505761671935-60b3a7427bad",
-desc:"Pueblo alpino hermoso.",
-color:"#ddd6fe"
+desc:"Pueblo alpino entre lago y montañas. Es considerado uno de los pueblos más bellos del mundo."
 }
 ];
 
@@ -35,14 +30,13 @@ let index = 0;
 const card = document.getElementById("card");
 
 function load(){
-    document.body.style.background = places[index].color;
-
     document.getElementById("img").src = places[index].img;
     document.getElementById("title").textContent = places[index].title;
     document.getElementById("titleBack").textContent = places[index].title;
     document.getElementById("desc").textContent = places[index].desc;
 }
 
+/* flip */
 card.addEventListener("click", ()=>{
     card.classList.toggle("flipped");
 });
@@ -61,4 +55,4 @@ document.getElementById("prev").onclick = ()=>{
     load();
 };
 
-load(); 
+load();
