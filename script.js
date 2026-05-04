f27644e45e4b68f4ab84bb9d0deedfa768f33daf
Mariabayonae <bayonam346@gmail.com>
2026-05-04 15:13:07 +0000

    intento 4

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
 const places = [
 {
-title:"Giethoorn, Países Bajos",
+title:"Giethoorn",
 img:"https://images.unsplash.com/photo-1500375592092-40eb2168fd21",
-desc:"Un pueblo sin carreteras donde todo se mueve en bote por canales."
+desc:"Pueblo sin carreteras, solo canales.",
+color:"#dbeafe"
 },
 {
-title:"Chefchaouen, Marruecos",
+title:"Chefchaouen",
 img:"https://images.unsplash.com/photo-1524492412937-b28074a5d7da",
-desc:"Ciudad azul escondida en las montañas del Rif."
+desc:"Ciudad azul en Marruecos.",
+color:"#c7d2fe"
 },
 {
-title:"Isla Socotra, Yemen",
+title:"Socotra",
 img:"https://images.unsplash.com/photo-1507525428034-b723cf961d3e",
-desc:"Paisajes únicos que parecen de otro planeta."
+desc:"Paisajes únicos del mundo.",
+color:"#fde68a"
 },
 {
-title:"Huacachina, Perú",
+title:"Huacachina",
 img:"https://images.unsplash.com/photo-1501785888041-af3ef285b470",
-desc:"Oasis natural rodeado de dunas en el desierto."
+desc:"Oasis en el desierto.",
+color:"#fed7aa"
 },
 {
-title:"Hallstatt, Austria",
+title:"Hallstatt",
 img:"https://images.unsplash.com/photo-1505761671935-60b3a7427bad",
-desc:"Pueblo alpino entre lago y montañas."
+desc:"Pueblo alpino hermoso.",
+color:"#ddd6fe"
 }
 ];
 
@@ -30,6 +35,8 @@ let index = 0;
 const card = document.getElementById("card");
 
 function load(){
+    document.body.style.background = places[index].color;
+
     document.getElementById("img").src = places[index].img;
     document.getElementById("title").textContent = places[index].title;
     document.getElementById("titleBack").textContent = places[index].title;
@@ -40,14 +47,18 @@ card.addEventListener("click", ()=>{
     card.classList.toggle("flipped");
 });
 
-function nextPlace(){
+/* next */
+document.getElementById("next").onclick = ()=>{
     index = (index + 1) % places.length;
     card.classList.remove("flipped");
     load();
+};
 
-    const btn = document.querySelector(".btn");
-    btn.classList.add("clicked");
-    setTimeout(()=>btn.classList.remove("clicked"),200);
-}
+/* prev */
+document.getElementById("prev").onclick = ()=>{
+    index = (index - 1 + places.length) % places.length;
+    card.classList.remove("flipped");
+    load();
+};
 
-load();
+load(); 
